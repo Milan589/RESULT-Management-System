ddb177256c145daa754f9dd26e58b0cce6348cb9
Aashish Pokharel <pokharellaashish454@gmail.com>
2022-03-29 22:45:50 +0545

    [modify]modifies the sql syntaxes

--- Exam_management_1648097497.sql
+++ Exam_management_1648097497.sql
@@ -25,6 +25,7 @@ CREATE TABLE teacher(
     teacher_is_admin VARCHAR(1),
 	teacher_year INT(10), 
 	teacher_dob DATE,
+    CONSTRAINT UNQ_temail UNIQUE(`teacher_username`);
     CONSTRAINT PK_teacher PRIMARY KEY (teacher_id),
     CONSTRAINT FKteacher_college FOREIGN KEY (college_id) REFERENCES college(college_id)
     );
@@ -53,6 +54,7 @@ CREATE TABLE student(
     student_address VARCHAR(150),
 	student_year INT(10),
 	student_dob DATE,
+    CONSTRAINT UNQ_email UNIQUE(`student_username`)
     CONSTRAINT FKstudent_college FOREIGN KEY(college_id) REFERENCES college(college_id) ,
     CONSTRAINT FKstudent_course FOREIGN KEY(course_id) REFERENCES course(course_id)
     );
@@ -62,6 +64,7 @@ CREATE TABLE exam(
     subject_id INT(10),
     exam_date DATE,
     exam_year INT,
+    semester VARCHAR(4),
     CONSTRAINT FKexam_subject FOREIGN KEY(subject_id) REFERENCES subjects(subject_id)
     );
     
@@ -72,8 +75,11 @@ CREATE TABLE RESULT(
     marks_obtained INT,
     grade VARCHAR(2),
     CONSTRAINT FKresult_student FOREIGN KEY(student_id) REFERENCES student(student_id),
-    CONSTRAINT FKresult_exam FOREIGN KEY(exam_id) REFERENCES exam(exam_id)
+    CONSTRAINT FKresult_exam FOREIGN KEY(exam_id) REFERENCES exam(exam_id),
+    CONSTRAINT UNIQUE (student_id, exam_id)
     );
+    );
+
 
 
 INSERT INTO course (course_id,course_name,course_description)
